85e4bae785b5236074a004b18477c486b8468752
Favour Onuoha <favour@teksol.com.gh>
2018-11-03 14:03:28 +0000

    fixed an error

--- models/connection.js
+++ models/connection.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 //mongoose.connect('mongodb://localhost/smsbackup');
-mongoose.connect('mongodb://elcentino:rock.roll@cluster0-shard-00-00-9byhr.mongodb.net:27017,cluster0-shard-00-01-9byhr.mongodb.net:27017,cluster0-shard-00-02-9byhr.mongodb.net:27017/vibe?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true/smsbackup');
+mongoose.connect('mongodb://elcentino:rock.roll@cluster0-shard-00-00-9byhr.mongodb.net:27017,cluster0-shard-00-01-9byhr.mongodb.net:27017,cluster0-shard-00-02-9byhr.mongodb.net:27017/smsbackup?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true');
 
 mongoose.connection
     .once('open', () => console.log('succeessfully connected to database'))
